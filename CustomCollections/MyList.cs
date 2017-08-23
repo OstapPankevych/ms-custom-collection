beb75347d05ef5a0de7ea57d241b7f2a29e2c6fe
OstapPankevych <ostappankevych@gmail.com>
2017-08-23 23:43:24 +0300

    custom list collection

---
+++ CustomCollections/MyList.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomCollections
+{
+    public class MyList<T> : ICollection<T>
+    {
+        protected int defaultElements = 10;
+
+        private const int StartCount = 0;
+
+        private int _count = StartCount; 
+        private T[] _elements;
+
+        public MyList()
+        {
+            Init();
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public void Add(T item)
+        {
+            if (_elements.Length <= _count)
+            {
+                _elements = _elements.Concat(new T[defaultElements]).ToArray();
+            }
+
+            _elements[_count++] = item;
+        }
+
+        public void Clear()
+        {
+            Init();
+        }
+
+        public bool Contains(T item)
+        {
+            foreach (var x in _elements)
+            {
+                if (!item.Equals(x)) continue;
+                return true;                
+            }
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            _elements.CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (var i = 0; i < _count; i++)
+            {
+                yield return _elements[i];        
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            var indexOf = Array.IndexOf(_elements, item);
+            if (indexOf == -1) return false;
+            for (var i = indexOf; i <= _count - 1; i++)
+            {
+                _elements[i] = _elements[i + 1];
+            }
+            _count--;
+            return true;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private void Init()
+        {
+            _elements = new T[defaultElements];
+            _count = StartCount;
+        }
+    }
+}
---
+++ CustomCollections/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace CustomCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new MyList<int>();
+            Console.WriteLine(list.Count);
+            list.Add(5);
+            list.Add(8);
+            list.Add(7);
+            list.Add(3);
+            list.Add(8);
+            list.Add(5);
+            list.Add(15);
+            list.Add(11);
+            list.Add(1);
+            list.Add(14);
+            list.Add(29);
+            list.Add(33);
+            list.Add(47);
+            list.Add(85);
+            list.Add(63);
+            list.Add(98);
+            list.Add(89);
+            list.Add(58);
+            list.Add(85);
+            list.Add(25);
+            list.Add(52);
+            list.Add(32);
+            Console.WriteLine(list.Count);
+
+            foreach (var x in list)
+            {
+                Console.WriteLine(string.Format("x = {0}", x));
+            }
+
+            list.Remove(8);
+            Console.WriteLine(list.Count);
+
+            foreach (var x in list)
+            {
+                Console.WriteLine(string.Format("x = {0}", x));
+            }
+
+            list.Clear();
+            Console.WriteLine(list.Count);
+
+            Console.ReadLine();
+        }
+    }
+}
